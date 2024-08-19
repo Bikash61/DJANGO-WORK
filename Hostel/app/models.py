d4a8f3e9394f8db4f00a28e6d300a6f8a3d52da7
Bikash61 <bashyalb96@gmail.com>
2024-08-19 11:36:50 +0545

    Update

--- Hostel/app/models.py
+++ Hostel/app/models.py
@@ -2,10 +2,12 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class User(models.Model):
+class UserDetails(models.Model):
     username = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=255)
+
+
 
     def __str__(self):
         return self.username

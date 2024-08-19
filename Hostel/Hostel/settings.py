77388d9554f2cccca39fa760113cd9d3dd840040
Bikash61 <bashyalb96@gmail.com>
2024-08-19 10:15:32 +0545

    Update

--- Hostel/Hostel/settings.py
+++ Hostel/Hostel/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app'
 ]
 
 MIDDLEWARE = [
@@ -68,7 +69,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'Hostel.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
--- Hostel/app/models.py
+++ Hostel/app/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class User(modes.Model):
+class User(models.Model):
     username = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.username
     

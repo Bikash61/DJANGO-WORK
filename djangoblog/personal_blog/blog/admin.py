31b3c0123e0200e5fa293bf9f5a56ca6397ca1ad
Bikash61 <bashyalb96@gmail.com>
2024-08-21 03:26:04 +0545

    Updating

--- djangoblog/personal_blog/blog/admin.py
+++ djangoblog/personal_blog/blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import *
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+class CommentAdmin(admin.ModelAdmin):
+    pass
 
 # Register your models here.
+admin.site.register(Post)
+admin.site.register(Category)
+admin.site.register(Comment)
--- djangoblog/personal_blog/blog/models.py
+++ djangoblog/personal_blog/blog/models.py
@@ -4,16 +4,27 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     body = models.TextField()
     created_on = models.DateTimeField(auto_now=True)
     last_modify = models.DateTimeField(auto_now=True)
     categories = models.ManyToManyField("Category", related_name='posts')
+    def __str__(self):
+        return self.title
 
 class Comment(models.Model):
     author = models.CharField(max_length=60)
     body = models.TextField()
     created_on = models.DateTimeField(auto_now=True)
     post = models.ForeignKey('Post', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.author} on '{self.post.title}'"
 

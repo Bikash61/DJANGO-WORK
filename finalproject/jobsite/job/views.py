8cfcb60f44c7572abaeba9b294a06aaae3fa2a3a
Bikash61 <bashyalb96@gmail.com>
2024-08-24 23:23:31 +0545

    Updates

--- finalproject/jobsite/job/views.py
+++ finalproject/jobsite/job/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'base.html')
 
+def login(request):
+    return render(request, 'log-in.html')
+    
+def signup(request):
+    return render(request, 'sign-up.html')

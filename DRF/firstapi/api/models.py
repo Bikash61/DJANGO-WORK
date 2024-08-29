78ad019bb00dfd7ac3cd82f96cf01628b2bb44fe
Bikash61 <bashyalb96@gmail.com>
2024-08-29 17:02:08 +0545

    UPdates

--- DRF/firstapi/api/models.py
+++ DRF/firstapi/api/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Students(Models.model):
+class Students(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
     email = models.EmailField(max_length=100)

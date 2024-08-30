33cf652339075c679287cdd3d1513b4a3a13fda3
Bikash61 <bashyalb96@gmail.com>
2024-08-31 03:44:54 +0545

    Updating the Ecom

---
+++ finalproject/ecommerce/app/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+
+class CustomerRegistrationForm(forms.Form):
+    first_name = forms.CharField(
+        max_length=30,
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'})
+    )
+    last_name = forms.CharField(
+        max_length=30,
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last Name'})
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email Address'})
+    )
+   
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'})
+    )
+    confirm_password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Confirm Password'})
+    )
--- finalproject/ecommerce/app/views.py
+++ finalproject/ecommerce/app/views.py
@@ -4,10 +4,16 @@ from django.views import View
 from django.http import HttpResponse
 from urllib import request
 from .models import Product
+from .forms import CustomerRegistrationForm
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
     return render(request, 'app/home.html')
+def about(request):
+    return render(request, 'app/about.html')
+def contact(request):
+    return render(request, 'app/contact.html')
 
 class CategoryView(View):
     def get(self, request, val):
@@ -26,3 +32,16 @@ class ProductDetail(View):
         product = Product.objects.get(pk=pk)
         return render(request, "app/productdetail.html", locals())
 
+class CustomerRegistrationView(View):
+    def get(self, request):
+        form = CustomerRegistrationForm()
+        return render(request, "app/customerregistration.html",locals())
+
+    def post(self, request):
+        form = CustomerRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.sucess(request, 'Congraluations! User Registered Sucessfully')
+        else:
+            messages.error(request, 'Invalid Input Data')
+            return render(request, "app/customerregistration.html",locals())

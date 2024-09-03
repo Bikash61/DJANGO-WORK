a4c651c16f7cfd7b06e8e251b4fffdd21be48e8f
Bikash61 <bashyalb96@gmail.com>
2024-09-03 21:50:47 +0545

    Updating the rendering part

--- saas/src/cfehome/views.py
+++ saas/src/cfehome/views.py
@@ -1,4 +1,10 @@
+import pathlib
 from django.http import HttpResponse
 
+this_dir = pathlib.Path(__file__).resolve().parent
+
 def home_page_view(request, *args, **kwargs):
-    return HttpResponse("<h1>Hello, world!</h1>")
+    html_ = ""
+    html_file_path = this_dir/"home.html"
+    html_ = html_file_path.read_text()
+    return HttpResponse(html_)

4ae6ec5a06ca685c2db03bd18466914169fa5c7e
Bikash61 <bashyalb96@gmail.com>
2024-09-04 17:09:35 +0545

    Updates

--- saas/src/cfehome/urls.py
+++ saas/src/cfehome/urls.py
@@ -21,5 +21,5 @@ from .views import home_page_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_page_view)
+    path('', home_page_view),
 ]
--- saas/src/cfehome/views.py
+++ saas/src/cfehome/views.py
@@ -1,26 +1,28 @@
 import pathlib
 from django.http import HttpResponse
 from django.shortcuts import render
+from visits.models import PageVisit
 
 this_dir = pathlib.Path(__file__).resolve().parent
 
 def home_page_view(request, *args, **kwargs):
+    queryset = PageVisit.objects.all()
     my_title = "My Page"
+ 
     my_context = {
-        'page_title': my_title
+        'page_title': my_title,
+        'pagevisit' : queryset.count()
     }
     html_template = "home.html"
+    PageVisit.objects.create()
     return render(request, html_template, my_context)
 
 
-# def old_home_page_view(request, *args, **kwargs):
+
+# def base_page_view(request, *args, **kwargs):
 #     my_title = "My Page"
 #     my_context = {
-#         'page_tile': my_title
+#         'page_title': my_title
 #     }
-#     html_ = ""
-
-
-#     # html_file_path = this_dir/"home.html"
-#     # html_ = html_file_path.read_text()
-#     return HttpResponse(html_)
+#     html_template = "base.html"
+#     return render(request, html_template, my_context)
